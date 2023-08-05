ed6fdb51ef36a3bc1365e8c1340abf9bffbcd943
MrRuslanYT <pandacorpglobal@gmail.com>
2023-08-05 16:50:42 +0300

    Added a loading text when the fact image is loading(While receiving it from Firebase and coin loading it)

--- app/src/main/java/com/pandacorp/knowui/presentation/ui/screens/MainScreen.kt
+++ app/src/main/java/com/pandacorp/knowui/presentation/ui/screens/MainScreen.kt
@@ -2,10 +2,8 @@ package com.pandacorp.knowui.presentation.ui.screens
 
 import android.content.res.Configuration
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -37,6 +35,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -49,7 +48,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import com.google.accompanist.drawablepainter.rememberDrawablePainter
 import com.pandacorp.knowui.R
 import com.pandacorp.knowui.domain.models.FactItem
 import com.pandacorp.knowui.domain.models.FactState
@@ -89,15 +87,19 @@ fun MainScreen(
             ) {
                 val facts = factsViewModel.facts.value
 
-                if(facts is FactState.Loading) {
-                    Pager(items = listOf(), isShowPlaceholder = true)
+                if (facts is FactState.Loading) {
+                    Pager(isShowPlaceholder = true)
                 } else {
-                    Pager(items = facts.data ?: emptyList(), isLoadMore = factsViewModel.isStopLoading.value, onLoadMore = {
-                        factsViewModel.loadMoreFacts()
-                    }, onFactClick = { fact ->
-                        currentFactViewModel.setFact(fact)
-                        navController!!.navigate(Constants.Screen.FACT)
-                    })
+                    Pager(
+                        items = facts.data ?: emptyList(),
+                        isLoadMore = factsViewModel.isStopLoading.value,
+                        onLoadMore = {
+                            factsViewModel.loadMoreFacts()
+                        },
+                        onFactClick = { fact ->
+                            currentFactViewModel.setFact(fact)
+                            navController!!.navigate(Constants.Screen.FACT)
+                        })
                     if (facts is FactState.Error) {
                         Snackbar(
                             modifier = Modifier
@@ -120,7 +122,7 @@ fun MainScreen(
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun Pager(
-    items: List<FactItem>,
+    items: List<FactItem> = emptyList(),
     isShowPlaceholder: Boolean = false,
     buffer: Int = 1,
     isLoadMore: Boolean = false,
@@ -129,12 +131,10 @@ private fun Pager(
 ) {
     val pagerState = rememberPagerState()
 
-    val facts: List<FactItem>
-    if (isShowPlaceholder) {
+    val facts: List<FactItem> = if (isShowPlaceholder) {
         val placeholderFact = FactItem()
-        facts = List(5) { placeholderFact }
+        List(5) { placeholderFact }
     } else {
-        facts = items
         if (isLoadMore) {
             val isAtTargetValue by remember(pagerState.currentPage) {
                 mutableStateOf(pagerState.currentPage == items.size - 1 - buffer)
@@ -143,6 +143,7 @@ private fun Pager(
                 if (isAtTargetValue) onLoadMore()
             }
         }
+        items
     }
 
     VerticalPager(
@@ -251,26 +252,40 @@ fun CardLandscapeContent(
     tags: List<String>,
     enableScroll: Boolean = false,
 ) {
+    var isLoading by remember {
+        mutableStateOf(true)
+    }
+
     Row(
         modifier = modifier
             .fillMaxSize()
             .padding(horizontal = 4.dp, vertical = 8.dp)
     ) {
-        if (imageUri == null) {
-            CardPlaceholderImage(
-                modifier = Modifier
-                    .fillMaxHeight(0.8f)
-                    .fillMaxWidth(0.4f)
-                    .align(Alignment.CenterVertically)
-            )
-        } else {
+        Box(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .then(
+                    if (isLoading) Modifier
+                        .shimmer(
+                            rememberShimmer(
+                                ShimmerBounds.View,
+                                theme = Animations.ShimmerTheme
+                            )
+                        ) else Modifier
+                )
+        ) {
+            if (isLoading) { // Show a loading text
+                Text(modifier = Modifier.align(Alignment.Center), text = stringResource(id = R.string.loading))
+            }
             AsyncImage(
                 modifier = Modifier
-                    .fillMaxHeight(0.8f)
-                    .fillMaxWidth(0.4f)
                     .padding(start = 8.dp)
-                    .align(Alignment.CenterVertically),
+                    .fillMaxHeight(0.8f)
+                    .fillMaxWidth(0.4f),
                 model = imageUri,
+                onSuccess = {
+                    isLoading = false
+                },
                 contentDescription = null,
             )
         }
@@ -306,25 +321,39 @@ fun CardPortraitContent(
     tags: List<String>,
     enableScroll: Boolean = false,
 ) {
+    var isLoading by remember {
+        mutableStateOf(true)
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        if (imageUri == null) {
-            CardPlaceholderImage(
-                modifier = Modifier
-                    .fillMaxHeight(0.4f)
-                    .fillMaxWidth(1f)
-                    .align(Alignment.CenterHorizontally)
-            )
-        } else {
+        Box(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .then(
+                    if (isLoading) Modifier
+                        .shimmer(
+                            rememberShimmer(
+                                ShimmerBounds.View,
+                                theme = Animations.ShimmerTheme
+                            )
+                        ) else Modifier
+                )
+        ) {
+            if (isLoading) { // Show a loading text
+                Text(modifier = Modifier.align(Alignment.Center), text = stringResource(id = R.string.loading))
+            }
             AsyncImage(
                 modifier = Modifier
                     .fillMaxHeight(0.4f)
-                    .fillMaxWidth(1f)
-                    .align(Alignment.CenterHorizontally),
+                    .fillMaxWidth(1f),
                 model = imageUri,
+                onSuccess = {
+                    isLoading = false
+                },
                 contentDescription = null,
             )
         }
@@ -347,19 +376,6 @@ fun CardPortraitContent(
             overflow = TextOverflow.Ellipsis
         )
     }
-}
-
-@Composable
-private fun CardPlaceholderImage(modifier: Modifier = Modifier) {
-    Image(
-        painter = rememberDrawablePainter(drawable = ColorDrawable(android.graphics.Color.DKGRAY)),
-        modifier = modifier
-            .padding(16.dp)
-            .shimmer(
-                rememberShimmer(ShimmerBounds.View, theme = Animations.ShimmerTheme)
-            ),
-        contentDescription = null,
-    )
 }
 
 @ExperimentalMaterial3Api
